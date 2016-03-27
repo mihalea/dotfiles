7dbfe2f54fad62e9bc89d8b1b1b923d24c9245fc
Mircea Mihalea <mircea@mihalea.ro>
2016-03-27 19:31:09 +0300

    Converted alias to script and added another alias
    sshen is now a standalone script
    added an alias for batch youtube downloading

--- zsh/.oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
+++ zsh/.oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
@@ -1,17 +1,9 @@
 #!/bin/zsh
 
-function sshen {
-	if [ ! -z $1 ];  then
-		file="$HOME/.ssh/$1"
-		if [ -s $file ]; then
-			ssh-add $file
-		fi
-	fi	
-}
-
 alias ls='ls --color=auto'
 alias l=ls
 alias ll='ls -l'
 alias la='ls -la'
 alias sudo='sudo '
 alias update-grub="grub-mkconfig -o /boot/grub/grub.cfg"
+alias batch-yt="youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)s' -a "
