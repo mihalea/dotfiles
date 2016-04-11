b7f00e56b422e164c19ce72d0e0211dfe97539ec
Mircea Mihalea <mircea@mihalea.ro>
2016-04-11 21:51:08 +0300

    Moved batch-yt from alias from script

--- zsh/.oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
+++ zsh/.oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
@@ -6,4 +6,3 @@ alias ll='ls -l'
 alias la='ls -la'
 alias sudo='sudo '
 alias update-grub="grub-mkconfig -o /boot/grub/grub.cfg"
-alias batch-yt="youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)s' -a "
