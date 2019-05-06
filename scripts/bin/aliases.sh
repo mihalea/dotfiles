2f8af72d5094520b01b9c0655e9ecb47bc511f15
Mircea Mihalea <mm8g15@soton.ac.uk>
2019-05-06 12:59:27 +0100

    Add mkcd and human aliases

--- scripts/bin/aliases.sh
+++ scripts/bin/aliases.sh
@@ -1,1 +1,13 @@
 function gi() { curl -sL https://www.gitignore.io/api/$@ ;}
+
+function mkcd () { mkdir "$1"; cd "$1"; }
+
+function human () { 
+	if [ -z "$1" ]; then
+		read value
+	else
+		value="$1"
+	fi
+
+	numfmt --to=iec-i --suffix=B --padding=7 "$value" 
+}
